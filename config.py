26759c8613e4a002021470b4c064d5f67a99fc05
kh-karhoe04 <jackhoe04@gmail.com>
2025-12-10 13:25:20 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.ch2soc4g61lg.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "123456789"
 customdb = "employee"
-custombucket = "chongkarhoe-employee1"
+custombucket = "chongkarhoe-employee"
 customregion = "us-east-1"
 
